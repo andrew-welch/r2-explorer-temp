6d3b4fefc8b0fd4411aecc20782d0114eb8f4863
Andrew Welch <19201714+andrew-welch@users.noreply.github.com>
2026-06-24 14:10:24 +1000

    Change readonly setting to false and add basicAuth

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import { R2Explorer } from "r2-explorer";
 
 export default R2Explorer({
 	// Set to false to allow users to upload files
-	readonly: true,
+	// readonly: true,
 
 	// Learn more how to secure your R2 Explorer instance:
 	// https://r2explorer.com/getting-started/security/
 	// cfAccessTeamName: "my-team-name",
+	readonly: false,
+	 basicAuth: {
+	 	username: "andrew",
+	 	password: "password",
+	 },
 });
